b2637c3e04d79debd6e9af2aa776ac9ed0950d67
Max Guclu <max.guclu@chasacademy.se>
2022-12-16 22:16:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndiduelltP_Banken_CS_MG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
